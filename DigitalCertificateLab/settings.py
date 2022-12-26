ce22b71d8ddd3a54243eafc24719a65a9cfcb7a0
varmtkaffe <kaffee.drivs@gmail.com>
2022-12-26 14:47:17 +0100

    regenerate secret key on start

--- DigitalCertificateLab/settings.py
+++ DigitalCertificateLab/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dv_cb+248z-g0)q^dgsf8tlmvrl(2hq@ki2ebdc3e+a7zlu5fy'
+SECRET_KEY = os.urandom(51)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
